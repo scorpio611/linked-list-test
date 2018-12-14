043b221a9921583edf5673266436079bf569512f
scorpio611 <evilmonter666@gmail.com>
2018-12-14 08:44:13 +0700

    [Thực hành] Triển khai lớp LinkedList đơn giản

---
+++ src/main/java/com/ngocnd/linked_list_test/MyLinkedListTest.java
@@ -0,0 +1,75 @@
+package com.ngocnd.linked_list_test;
+
+public class MyLinkedListTest {
+    static class MyLinkedList {
+        private Node head;
+        private int numNodes=0;
+
+        public MyLinkedList(Object data) {
+            head = new Node(data);
+        }
+
+        private class Node {
+            private Node next;
+            private Object data;
+
+            public Node(Object data) {
+                this.data = data;
+            }
+
+            public Object getData() {
+                return data;
+            }
+        }
+
+        public void add(int index, Object data) {
+            Node temp = head;
+            Node holder;
+
+            for (int i = 0; i < index - 1 && temp.next != null; i++) {
+                temp = temp.next;
+            }
+
+            holder = temp.next;
+            temp.next = new Node(data);
+            temp.next.next = holder;
+            numNodes++;
+        }
+
+        public void addFirst(Object data) {
+            Node temp = head;
+            head = new Node(data);
+            head.next = temp;
+            numNodes++;
+        }
+
+        public Node get(int index) {
+            Node temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+        public void printList() {
+            Node temp = head;
+            while (temp!=null){
+                System.out.println(temp.data);
+                temp=temp.next;
+            }
+        }
+    }
+
+        public static void main(String[] args) {
+            System.out.println("/=/=/=/= TESTING /=/=/=/=");
+            MyLinkedList ll = new MyLinkedList(10);
+            ll.addFirst(11);
+            ll.addFirst(16);
+            ll.addFirst(13);
+
+            ll.add(4,9);
+            ll.add(4,12);
+            ll.printList();
+        }
+
+}
